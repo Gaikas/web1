cfc020862485d25998df71aca17db0ada2710d87
Halina Nareika <gaikas9292@gmail.com>
2023-03-21 19:39:25 +0300

    Add new pages

--- siteD/views.py
+++ siteD/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Привет Дима</h1>")
+    return render(request, "siteD/index.html")
 
 def about(request):
-    return HttpResponse("<h2>Вторая страница</h2>")
+    return render(request, "siteD/about.html")
