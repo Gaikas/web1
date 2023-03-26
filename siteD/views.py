6bf015ec66f417ea1d7acb7f705f452e904878fc
Halina Nareika <gaikas9292@gmail.com>
2023-03-26 15:57:31 +0300

    Add info from admin

--- siteD/views.py
+++ siteD/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def index(request):
-    return render(request, "siteD/index.html")
+    zap1 = {
+        'title1': 'Главная страница',
+        'tasks' : models.Task.objects.all()
+    }
+    return render(request, "siteD/index.html", zap1)
 
 def about(request):
     return render(request, "siteD/about.html")
