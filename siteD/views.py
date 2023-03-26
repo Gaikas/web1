190949a6cc48d7d783185afb20667ca1c731a99f
Halina Nareika <gaikas9292@gmail.com>
2023-03-26 16:51:38 +0300

    Add new page with task

--- siteD/views.py
+++ siteD/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
+from .forms import TaskForm
 
 def index(request):
     zap1 = {
         'title1': 'Главная страница',
-        'tasks' : models.Task.objects.all()
+        'tasks' : models.Task.objects.order_by('-id')
     }
     return render(request, "siteD/index.html", zap1)
 
 def about(request):
     return render(request, "siteD/about.html")
+
+def create(request):
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, "siteD/create.html")
