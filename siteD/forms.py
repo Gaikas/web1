3c1dda40b248faa1b3e322a5ad741617ed9cd851
Halina Nareika <gaikas9292@gmail.com>
2023-03-26 17:15:29 +0300

    Add working field in the form

--- siteD/forms.py
+++ siteD/forms.py
@@ -1,7 +1,18 @@
-from . models import Task
-from django.forms import ModelForm
 
-class TaskForm(ModelForm):
+from . models import Task
+from django.forms import ModelForm, TextInput
+
+class TaskForm (ModelForm):
     class Meta:
         model = Task
         fields = ["title", "task"]
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите название'
+            }),
+            "task": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите название'
+            }),
+        }
